40c34582bc0cfb7b5fa1ba46e4aadd5755269c3f
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-29 20:23:16 +0100

    Thinking wrong I guess. Too tired to be productive.

---
+++ Onatrix/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Onatrix.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Your Name is required.")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Email address is required.")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^(([^<>()\[\]\\.,;:\s@\""]+(\.[^<>()\[\]\\.,;:\s@\""]+)*)|(\"".+\""))@((\[[0-9]{1,3}(\.[0-9]{1,3}){3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$",
+        ErrorMessage = "Please enter a valid email address.")]
+    public string Email { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Phone number is required.")]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Please select an option.")]
+    public string SelectedOption { get; set; } = null!;
+}
---
+++ Onatrix/Views/Partials/Forms/_CallbackForm.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Onatrix.Views.Partials.Forms;
+
+public class _CallbackForm : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
