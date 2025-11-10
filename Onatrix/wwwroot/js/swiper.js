b83ef85ed529ec6d7181a0d0eb3c425d2290ca57
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-11-10 10:46:20 +0100

    Added pagination functionality, and some slide positioning in swiper.js

--- Onatrix/wwwroot/js/swiper.js
+++ Onatrix/wwwroot/js/swiper.js
@@ -3,5 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
         slidesPerView: 1.2,
         spaceBetween: 16,
         centeredSlides: true,
+        centeredSlidesBounds: true,
+        slidesOffsetBefore: 16,
+        slidesOffsetAfter: 16,
+        
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: (i, className) => `<span class="${className}">${i + 1}</span>`
+        },
+        
+        navigation: {
+            nextEl: '.custom-next',
+            prevEl: '.custom-prev',
+        }
     })
 })
