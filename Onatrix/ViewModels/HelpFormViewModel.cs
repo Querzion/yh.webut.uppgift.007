9625976bb05c4408960bb9f128ee2e3a14de4d20
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-11-10 14:50:55 +0100

    Added the HelpFormViewModel.cs.

---
+++ Onatrix/ViewModels/HelpFormViewModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Onatrix.ViewModels;
+
+public class HelpFormViewModel
+{
+    [Required(ErrorMessage = "Email address is required.")]
+    [Display(Name = "Email address", Prompt = "Enter your email address")]
+    [RegularExpression(@"^(([^<>()\[\]\\.,;:\s@\""]+(\.[^<>()\[\]\\.,;:\s@\""]+)*)|(\"".+\""))@((\[[0-9]{1,3}(\.[0-9]{1,3}){3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$",
+        ErrorMessage = "Please enter a valid email address.")]
+    public string Email { get; set; } = null!;
+}
